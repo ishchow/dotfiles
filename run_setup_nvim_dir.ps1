b6952b2a777758a364b2fdb41505f870d5646214
Ishaat Chowdhury <ishaat@ualberta.ca>
2022-03-25 16:03:19 -0700

    Delete run_setup_nvim_dir.ps1

--- run_setup_nvim_dir.ps1
+++
@@ -1,6 +0,0 @@
-$linkPath = "C:\Users\$($env:UserName)\AppData\Local\nvim"
-
-if (-not $(Test-Path $linkPath))
-{
-    New-Item -ItemType Junction -Path $linkPath -Target "C:\Users\$($env:Username)\.config"
-}
