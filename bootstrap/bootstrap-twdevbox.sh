089957daaca8d34600a5a330186f6b37a0e991de
Ishaat Chowdhury <ishaat@ualberta.ca>
2023-01-30 20:44:42 -0800

    Run fish as interactive shell and fix devbox bs

--- bootstrap/bootstrap-twdevbox.sh
+++ bootstrap/bootstrap-twdevbox.sh
@@ -3,27 +3,26 @@ source ./bootstrap.sh
 
 echo "Adding repositories..."
 if ! $(zypper lr | grep "vscode" &> /dev/null); then
-    sudo zypper ar -p 105 "https://packages.microsoft.com/yumrepos/vscode vscode"
+    echo "Adding vscode repo..."
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    sudo zypper addrepo -p 105 https://packages.microsoft.com/yumrepos/vscode vscode
 fi
 
 echo "Installing packages..."
 sudo zypper ref
 sudo zypper in -y \
-	code \
+    code \
     libfuse2 \
-    libfuse2-32bit \
-    firefox
+    libfuse2-32bit
 
 echo "Exporting binaries, apps, and services to host system..."
-distrobox-export --app code --extra-flags "--foreground"
+if command -v code &> /dev/null; then
+    distrobox-export --app code --extra-flags "--foreground"
+fi
 distrobox-export --bin $(which nvim) --export-path ~/.local/bin
-distrobox-export --bin $(which git) --export-path ~/.local/bin
 
 if test ~/.local/share/JetBrains/Toolbox/scripts/idea; then
     distrobox-export \
         --bin ~/.local/share/JetBrains/Toolbox/scripts \
         --export-path ~/.local/bin
 fi
-
-echo "Installing konsave..."
-sudo python3 -m pip install konsave
