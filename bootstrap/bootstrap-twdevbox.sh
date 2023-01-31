ce31430b9fb2eb765d3d829d208bb22c5222ccaf
Ishaat Chowdhury <ishaat@ualberta.ca>
2023-01-30 21:54:40 -0800

    Fix devbox JB bootstrap and fish bashrc

--- bootstrap/bootstrap-twdevbox.sh
+++ bootstrap/bootstrap-twdevbox.sh
@@ -8,12 +8,17 @@ if ! $(zypper lr | grep "vscode" &> /dev/null); then
     sudo zypper addrepo -p 105 https://packages.microsoft.com/yumrepos/vscode vscode
 fi
 
+echo "Installing patterns..."
+sudo zypper in --no-recommends -t pattern x11
+
 echo "Installing packages..."
 sudo zypper ref
 sudo zypper in -y \
     code \
     libfuse2 \
-    libfuse2-32bit
+    libfuse2-32bit \
+    libgthread-2_0-0 \
+    libXtst6
 
 echo "Exporting binaries, apps, and services to host system..."
 if command -v code &> /dev/null; then
