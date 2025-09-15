026ca26bc744d051e5e42996db44be0f9c94b4dd
Ishaat Chowdhury <ishaatchowdhury@gmail.com>
2025-09-14 21:33:44 -0700

    Update windows bootstrap

--- bootstrap/bootstrap.ps1
+++ bootstrap/bootstrap.ps1
@@ -13,15 +13,6 @@ function Check-Command($cmdname)
     return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
 }
 
-function Install-WingetPkg($appName)
-{
-    $app = $(winget list $appName | Select-String "No installed package found")
-    if ($app)
-    {
-        winget install $appName
-    }
-}
-
 Write-Header "Disable Sleep on AC Power..."
 Powercfg /Change standby-timeout-ac 0
 
@@ -46,58 +37,13 @@ foreach ($uwp in $uwpApps)
     Get-AppxPackage -Name $uwp | Remove-AppxPackage
 }
 
-Write-Header "Enable Windows 10 Developer Mode..."
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /t REG_DWORD /f /v "AllowDevelopmentWithoutDevLicense" /d "1"
-
-Write-Header "Enable Remote Desktop..."
-Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\" -Name "fDenyTSConnections" -Value 0
-Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\" -Name "UserAuthentication" -Value 1
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-
-if (!$(Check-Command "choco"))
-{
-    Write-Header "Installing Chocolatey"
-    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-}
-
-Write-Header "Installing Base Chocolatey packages"
-choco install -y .\packages-base.config
-
-Write-Header "Installing Windows Features"
-choco install -y .\features.config -s windowsFeatures
-
 Write-Header "Installing winget packages"
-Install-WingetPkg Git.Git
-Install-WingetPkg Microsoft.PowerToys
-Install-WingetPkg Microsoft.WindowsTerminal
-Install-WingetPkg Microsoft.PowerShell
-Install-WingetPkg Microsoft.VisualStudioCode
-Install-WingetPkg 'openSUSE Tumbleweed'
-Install-WingetPkg 7zip.7zip
-Install-WingetPkg voidtools.Everything
-Install-WingetPkg Neovim.Neovim
-Install-WingetPkg Obsidian.Obsidian
-
-Write-Header "Installing scoop packages..."
-scoop bucket add main
-scoop install gsudo
-scoop install bat
-scoop install ripgrep
-scoop install fzf
+winget import -i .\winget-packages.json
 
 Write-Header "Installing powershell modules"
 Install-Module -Name PowerShellGet -Force
 Install-Module ZLocation
 Install-Module -Name PSFzf
-
-Write-Header "Setting up gsudo"
-gsudo config CacheMode auto
-
-if (Check-Command "wsl")
-{
-	Write-Header "Setting WSL 2 as default version"
-	wsl --set-default-version 2
-}
 
 $p = [System.Environment]::GetEnvironmentVariable('Path',[System.EnvironmentVariableTarget]::Machine)
 if (!$p.Contains("C:\Program Files\Alt-Tab Terminator"))
@@ -118,3 +64,6 @@ if (Get-Command code)
 {
     Get-Content ~/.local/share/chezmoi/misc/vscode/extensions.txt | ForEach-Object { Invoke-Expression "code --install-extension $_" }
 }
+
+Write-Header "Setup wsl"
+wsl --install --distribution openSUSE-Tumbleweed
