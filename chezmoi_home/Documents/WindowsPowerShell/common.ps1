03fa7fbff630e2008f42b14cc453cfe5a5ac3367
Ishaat Chowdhury <ishaatchowdhury@gmail.com>
2025-10-01 22:32:34 -0600

    Update powershell profile even more

---
+++ chezmoi_home/Documents/WindowsPowerShell/common.ps1
@@ -0,0 +1,56 @@
+Import-Module PSReadLine
+Import-Module ZLocation
+
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# In Emacs mode - Tab acts like in bash, but the Windows style completion
+# is still useful sometimes, so bind some keys so we can do both
+Set-PSReadLineKeyHandler -Key Ctrl+q -Function TabCompleteNext
+Set-PSReadLineKeyHandler -Key Ctrl+Q -Function TabCompletePrevious
+
+# Clipboard interaction is bound by default in Windows mode, but not Emacs mode.
+Set-PSReadLineKeyHandler -Key Ctrl+C -Function Copy
+Set-PSReadLineKeyHandler -Key Ctrl+v -Function Paste
+
+if (Get-Command fzf -ErrorAction "silentlycontinue")
+{
+    Import-Module -Name PSFzf
+    Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+    Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+}
+
+if (Get-Command fnm -ErrorAction "silentlycontinue")
+{
+    fnm env --use-on-cd | Out-String | Invoke-Expression
+}
+
+function Show-Notification {
+    [cmdletbinding()]
+    Param (
+        [string]
+        $ToastTitle,
+        [string]
+        [parameter(ValueFromPipeline)]
+        $ToastText
+    )
+
+    [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
+    $Template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText02)
+
+    $RawXml = [xml] $Template.GetXml()
+    ($RawXml.toast.visual.binding.text|where {$_.id -eq "1"}).AppendChild($RawXml.CreateTextNode($ToastTitle)) > $null
+    ($RawXml.toast.visual.binding.text|where {$_.id -eq "2"}).AppendChild($RawXml.CreateTextNode($ToastText)) > $null
+
+    $SerializedXml = New-Object Windows.Data.Xml.Dom.XmlDocument
+    $SerializedXml.LoadXml($RawXml.OuterXml)
+
+    $Toast = [Windows.UI.Notifications.ToastNotification]::new($SerializedXml)
+    $Toast.Tag = "PowerShell"
+    $Toast.Group = "PowerShell"
+    $Toast.ExpirationTime = [DateTimeOffset]::Now.AddMinutes(1)
+
+    $Notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("PowerShell")
+    $Notifier.Show($Toast);
+}
