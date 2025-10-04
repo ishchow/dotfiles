3334cd3a2976f80a12d143664e6117094a4559c7
Ishaat Chowdhury <ishaatchowdhury@gmail.com>
2025-10-04 03:41:55 +0000

    Start ssh agent in shell config

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,4 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Start ssh-agent if not already running
+if not set -q SSH_AUTH_SOCK
+    eval (ssh-agent -c)
+end
+
 zoxide init fish | source
