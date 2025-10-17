6f5d2a036e9c8e7761d4a48279e277919d18d6d6
Ishaat Chowdhury <ishaatchowdhury@gmail.com>
2025-10-17 08:38:00 -0600

    Update windows bootstrap

--- chezmoi_home/AppData/Local/ishaat/bootstrap/000_bootstrap.ps1
+++ chezmoi_home/AppData/Local/ishaat/bootstrap/000_bootstrap.ps1
@@ -1,12 +1,12 @@
-# Run as admin
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+# Throw if run as admin
+if (([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { throw "Cannot run the bootstrap script as Admin!" }
 
-powershell.exe -File "$($PSScriptRoot)\001_basic_setup.ps1"
-powershell.exe -File "$($PSScriptRoot)\002_setup_ssh.ps1"
-powershell.exe -File "$($PSScriptRoot)\003_setup_ps_profile.ps1"
-powershell.exe -File "$($PSScriptRoot)\004_start_sshd.ps1"
+sudo powershell.exe -File "$($PSScriptRoot)\001_basic_setup.ps1"
+sudo powershell.exe -File "$($PSScriptRoot)\002_setup_ssh.ps1"
+sudo powershell.exe -File "$($PSScriptRoot)\003_setup_ps_profile.ps1"
+sudo powershell.exe -File "$($PSScriptRoot)\004_start_sshd.ps1"
 powershell.exe -File "$($PSScriptRoot)\005_install_packages.ps1"
-powershell.exe -File "$($PSScriptRoot)\006_setup_path.ps1"
-powershell.exe -File "$($PSScriptRoot)\007_setup_wsl.ps1"
-powershell.exe -File "$($PSScriptRoot)\008_setup_kanata.ps1"
-powershell.exe -File "$($PSScriptRoot)\009_install_ahk_v1.ps1"
+sudo powershell.exe -File "$($PSScriptRoot)\006_setup_path.ps1"
+sudo powershell.exe -File "$($PSScriptRoot)\007_setup_wsl.ps1"
+sudo powershell.exe -File "$($PSScriptRoot)\008_setup_kanata.ps1"
+sudo powershell.exe -File "$($PSScriptRoot)\009_install_ahk_v1.ps1"
